e4020bffac6f709b7ceb4bb43980d9f64d8f3d99
Edgar <nazyrov01@gmail.com>
2021-12-19 22:48:52 +0500

    Initial Commit

---
+++ TRPGHelper/TRPGHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  TRPGHelperApp.swift
+//  TRPGHelper
+//
+//  Created by Эдгар Назыров on 19.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct TRPGHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
